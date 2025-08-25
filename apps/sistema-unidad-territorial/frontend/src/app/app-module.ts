9e437586f3bb60f06246bca1fb2321bbf479651c
Vincenzo Orellana <vin.orellana@duocuc.cl>
2025-08-24 23:02:42 -0400

    feat(auth): implementar autenticación y rutas administrativas, incluyendo guardias y componentes de inicio de sesión

--- apps/sistema-unidad-territorial/frontend/src/app/app-module.ts
+++ apps/sistema-unidad-territorial/frontend/src/app/app-module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { App } from './app';
 import { appRoutes } from './app.routes';
-import { NxWelcome } from './nx-welcome';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './shared/auth/auth.interceptor';
 
 @NgModule({
-  declarations: [App, NxWelcome],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [provideBrowserGlobalErrorListeners()],
-  bootstrap: [App],
+	declarations: [App],
+	imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideHttpClient(withInterceptors([authInterceptor])),
+	],
+	bootstrap: [App],
 })
 export class AppModule {}
