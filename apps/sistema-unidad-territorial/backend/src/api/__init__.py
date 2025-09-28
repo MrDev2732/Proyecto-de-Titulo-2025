f5476c2e1f6a0a7bd70ae7169cb8d5e401e729c7
Vincenzo Orellana <vin.orellana@duocuc.cl>
2025-09-28 04:14:04 -0300

    feat(rutas): agregar routers para la comunidad y archivos, ampliando la funcionalidad del sistema de unidad territorial.

--- apps/sistema-unidad-territorial/backend/src/api/__init__.py
+++ apps/sistema-unidad-territorial/backend/src/api/__init__.py
@@ -1,6 +1,10 @@
 from src.api.auth import router as auth_router
+from src.api.community import router as community_router
+from src.api.files import router as files_router
 
 
 __all__ = [
-    "auth_router"
+    "auth_router",
+    "community_router",
+    "files_router"
 ]
