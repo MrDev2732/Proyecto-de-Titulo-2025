366b61f8dfc2e78f4aebc62679633b2801e1bc52
Vincenzo Orellana <vin.orellana@duocuc.cl>
2025-10-09 03:48:51 -0300

    fix(auth.guard): update navigation to redirect users to the signin page instead of the login page

--- apps/sistema-unidad-territorial/frontend/src/app/shared/auth/auth.guard.ts
+++ apps/sistema-unidad-territorial/frontend/src/app/shared/auth/auth.guard.ts
@@ -6,7 +6,7 @@ export const authGuard: CanActivateFn = () => {
 	const auth = inject(AuthService);
 	const router = inject(Router);
 	if (!auth.isAuthenticated()) {
-		void router.navigate(['/auth/login']);
+		void router.navigate(['/signin']);
 		return false;
 	}
 	return true;
